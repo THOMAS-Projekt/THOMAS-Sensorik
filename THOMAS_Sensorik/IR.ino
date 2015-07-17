5124d618a52e07b39c6b6e9cf860f7f786345201
Marcus Wichelmann <marcus.wichelmann@hotmail.de>
2015-07-17 18:46:30 +0200

    IR-Codes überschrieben (Vermutlich wurde die Library angepasst)

--- THOMAS_Sensorik/IR.ino
+++ THOMAS_Sensorik/IR.ino
@@ -2,44 +2,6 @@
 // Port für die Datenleitung des IR-Sensors
 #define IR_rec_port 44
 
-// Infrarot-Befehle
-#define IR_mute "270d"
-#define IR_power "270c"
-#define IR_red "276d"
-#define IR_green "276e"
-#define IR_yellow "276f"
-#define IR_textup "2770"
-#define IR_fav "2784"
-#define IR_window "274b" // Funktion: Zustände aktualisieren
-#define IR_onetwo "274e"
-#define IR_textdown "273c"
-#define IR_guide "27cc"
-#define IR_info "27cb"
-#define IR_timer "2775"
-#define IR_menu "2754"
-#define IR_top "2758"
-#define IR_back "2783"
-#define IR_left "275a"
-#define IR_OK "275c"
-#define IR_right "275b"
-#define IR_volumeup "2710"
-#define IR_bottom "2759"
-#define IR_channelup "2720"
-#define IR_volumedown "2711"
-#define IR_channeldown "2721"
-#define IR_one "2701"
-#define IR_two "2702"
-#define IR_three "2703"
-#define IR_four "2704"
-#define IR_five "2705"
-#define IR_six "2706"
-#define IR_seven "2707"
-#define IR_eight "2708"
-#define IR_nine "2709"
-#define IR_tvstb "2743"
-#define IR_zero "2700"
-#define IR_radio "27b0"
-
 // ++++++++++++++++++++<[ VARIABELN ]>++++++++++++++++++++
 // Infrarot-Empfänger
 IRrecv irrecv(IR_rec_port);
@@ -87,32 +49,32 @@ bool IR_process()
 	String data = String(IR_read(), HEX);
 
 	// Daten empfangen => Inhalt verarbeiten
-	if(data == IR_window)
+	if(data == "cd42" || data == "48b4")
 	{
 		// Sensorzustände aktualisieren
 		US_reload_all();
 	}
-	else if(data == IR_right)
+	else if(data == "c2a6" || data == "5830")
 	{
 		// Nächste Menüseite
 		MU_next_page();
 	}
-	else if(data == IR_left)
+	else if(data == "c439" || data == "59c3")
 	{
 		// Vorherige Menüseite
 		MU_previous_page();
 	}
-	else if(data == IR_power)
+	else if(data == "c74a" || data == "8adc")
 	{
 		// Motorsteuerung umschalten
 		RL_toggle_motor_control();
 	}
-	else if(data == IR_channelup)
+	else if(data == "87fe" || data == "fab0")
 	{
 		// Servo nach rechts drehen
 		SV_cam_increase_degree(10);
 	}
-	else if(data == IR_channeldown)
+	else if(data == "4d8f" || data == "606d")
 	{
 		// Servo nach links drehen
 		SV_cam_decrease_degree(10);
